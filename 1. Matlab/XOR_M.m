1c2d21f03e7f86ddc18b894765823509ab49acac
Jose David <80493134+jdalvaradocol@users.noreply.github.com>
2023-06-10 10:46:37 -0500

    Update XOR_M.m

--- 1. Matlab/XOR_M.m
+++ 1. Matlab/XOR_M.m
@@ -1,44 +1,119 @@
 
-
 clc
 close all
 clear all
 
+% Declaramos las variables de entrada
 x1 = 0; x2 = 1;
- b = 1;  f = 0;
 
+% Declaramos el valor del bias (b = 1)
+b = 1;  
+
+% Declaramos el valor del funciona de activacion (f = 0)
+f = 0;
+
+% Declaramos el vector de entrada con el bias
 X1 = [ b ; x1 ; x2]
+
+% Declaramos los areglos de los pesos de las neuronas W1 y W2  
 W1 = [-0.5 1.5; 1 -1; 1 -1]
 W2 = [   -1.5 ;    1;    1]
 
-y1 = [  X1(1,1)*W1(1,1) + X1(2,1)*W1(2,1) + X1(3,1)*W1(3,1) ;
-        X1(1,1)*W1(1,2) + X1(2,1)*W1(2,2) + X1(3,1)*W1(3,2)]
+% Realizamos la operacion de la primera capa
+% entrada es X1, los pesos W1 y la salida xr1 = f(X1 * W1)
+
+y1 = [ X1(1,1)*W1(1,1) + X1(2,1)*W1(2,1) + X1(3,1)*W1(3,1) ;
+       X1(1,1)*W1(1,2) + X1(2,1)*W1(2,2) + X1(3,1)*W1(3,2)]
+
+% Aplicamos la funcion de activacion.
 Xr1 = y1 > f
 
+% Declaramos el vector de entrada de la segunda capa.
 X2 = [ b ; Xr1]
 
+% Realizamos la operacion de segunda capa
+% entrada es X2, los pesos W2 y la salida xr2 = f(X2 * W2)
 y2 = W2(1,1)*X2(1,1) + W2(2,1)*X2(2,1) + W2(3,1)*X2(3,1)    
+
+% Aplicamos la funciona de activacion.
 Xr2 = y2 > f  
 
+% salida de la RN
 y = Xr2
 
 clc
 close all
 clear all
 
-x1 = 0; x2 = 0;
- b = 1;  f = 0;
+% Declaramos las variables de entrada
+x1 = 0; x2 = 1;
 
+% Declaramos el valor del bias (b = 1)
+b = 1;  
+
+% Declaramos el valor del funciona de activacion (f = 0)
+f = 0;
+
+% Declaramos el vector de entrada con el bias
 X1 = [ b ; x1 ; x2]
-W1 = [-0.5 1.5; 1 -1; 1 -1]
-W2 = [   -1.5 ;    1;    1]
 
-[num_c num_f] = size(W1)
+% Declaramos los areglos de los pesos de las neuronas W1 y W2  
+W1 = [-0.5 1.5; 1 -1; 1 -1];
+W2 = [   -1.5 ;    1;    1];
 
-for i=1 : num_f
-    y1(i,1) = 0
-    for j=1 : num_c
-        y1(i,1) = y1(i,1) + (X1(j,1) * W1(j,i))
+% Realizamos la operacion de la primera capa
+% entrada es X1, los pesos W1 y la salida xr1 = f(X1 * W1)
+
+[num_f num_c] = size(W1);
+
+for i=1 : num_c
+    y1(i,1) = 0;
+    for j=1 : num_f
+        y1(i,1) = y1(i,1) + (X1(j,1) * W1(j,i));
     end
 end
+
+% Aplicamos la funcion de activacion.
+
+[num_f num_c] = size(y1);
+
+for i=1 : num_f
+    if y1(i,1) > f 
+        Xr1(i,1) = 1;
+    else
+        Xr1(i,1) = 0;
+    end    
+end
+
+% Declaramos el vector de entrada de la segunda capa.
+X2 = [ b ; Xr1];
+
+% Realizamos la operacion de segunda capa
+% entrada es X2, los pesos W2 y la salida xr2 = f(X2 * W2)
+
+[num_f num_c] = size(W2);
+
+for i=1 : num_c
+    y2(i,1) = 0;
+    for j=1 : num_f
+        y2(i,1) = y2(i,1) + (X2(j,1) * W2(j,i));
+    end
+end
+
+% Aplicamos la funcion de activacion.
+
+[num_f num_c] = size(y2);
+
+for i=1 : num_f
+    if y2(i,1) > f 
+        Xr2(i,1) = 1;
+    else
+        Xr2(i,1) = 0;
+    end    
+end 
+
+% salida de la RN
+y = Xr2
+
+fprintf("La salida de la RN es igual a %d", y)
 
